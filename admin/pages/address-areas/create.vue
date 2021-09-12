f97067beda33197b67cb9817749f46a26f49a11e
Jason Chan <jasonnchann24@gmail.com>
2021-09-12 23:54:29 +0700

    address area crud done

---
+++ admin/pages/address-areas/create.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Add New Area</h1>
+    <AddressAreasForm />
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+}
+</script>
--- api/database/seeders/Fake/FakeAddressAreaSeeder.php
+++ api/database/seeders/Fake/FakeAddressAreaSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\Fake;
 
 use App\Imports\AddressAreaImport;
+use App\Models\AddressArea;
 use Illuminate\Database\Seeder;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -15,6 +16,30 @@ class FakeAddressAreaSeeder extends Seeder
      */
     public function run()
     {
-        Excel::import(new AddressAreaImport, 'data-imports/kode-pos.csv', 'local');
+        $areas = [
+            [
+                'name' => 'Jakarta Utara',
+                'description' => 'This covers Kelapa Gading, Sunter',
+                'small_price' => 10000,
+                'medium_price' => 15000
+            ],
+            [
+                'name' => 'Jakarta Barat',
+                'description' => 'This covers Kebon Jeruk, Puri, Tanjung Duren',
+                'small_price' => 13000,
+                'medium_price' => 17000
+            ],
+            [
+                'name' => 'Jakarta Utara 2',
+                'description' => 'This covers PIK, Pluit',
+                'small_price' => 5000,
+                'medium_price' => 10000
+            ],
+        ];
+        // Excel::import(new AddressAreaImport, 'data-imports/kode-pos.csv', 'local');
+
+        foreach ($areas as $area) {
+            AddressArea::create($area);
+        }
     }
 }
