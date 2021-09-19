96d7e4113bf49db49a0fe6d7aec82978789e90ce
Jason Chan <jasonnchann24@gmail.com>
2021-09-20 02:34:31 +0700

    fix

--- api/app/Http/Controllers/PaymentHookController.php
+++ api/app/Http/Controllers/PaymentHookController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use App\Services\Contracts\PaymentGatewayContract;
 use Illuminate\Http\Request;
+use Midtrans\Config as MidtransConfig;
+
 
 class PaymentHookController extends Controller
 {
 
-    protected $paymentGateway;
+    // protected $paymentGateway;
 
-    public function __construct(PaymentGatewayContract $paymentGatewayContract)
-    {
-        $this->paymentGateway = $paymentGatewayContract;
-    }
+    // public function __construct(PaymentGatewayContract $paymentGatewayContract)
+    // {
+    //     $this->paymentGateway = $paymentGatewayContract;
+    // }
 
     /**
      * Handle the incoming request.
@@ -23,10 +26,35 @@ class PaymentHookController extends Controller
      */
     public function __invoke(Request $request)
     {
-        try {
-            $this->paymentGateway->updateInvoiceStatus($request);
-        } catch (\Exception $e) {
-            return response()->json(['errors' => $e->getMessage()], 400);
+        // try {
+        //     $this->paymentGateway->updateInvoiceStatus($request);
+        // } catch (\Exception $e) {
+        //     return response()->json(['errors' => $e->getMessage()], 400);
+        // }
+        MidtransConfig::$isProduction = config('app.middtrans_production');
+        MidtransConfig::$serverKey = config('app.midtrans_server_key');
+
+        $notif = new \Midtrans\Notification();
+
+        $transactionStatus = $notif->transaction_status;
+        $order_id = $notif->order_id;
+
+        $invoice = Invoice::findOrFail($order_id);
+
+        $payload = json_decode($request->getContent());
+        $validSignatureKey = hash("sha512",  $payload->order_id . $payload->status_code . $payload->gross_amount . config('app.midtrans_server_key'));
+        if ($payload->signature_key != $validSignatureKey) {
+            return response()->json(['message' => 'Invalid signature'], 403);
         }
+
+        // $invoice->invoicePayment()->updateOrCreate(
+        //     ['invoice_id' => $order_id],
+        //     (array) $notif
+        // );
+
+        $invoice->status = $transactionStatus;
+        $invoice->save();
+
+        return true;
     }
 }
